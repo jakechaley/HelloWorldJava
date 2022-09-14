a8a62737ba3f80df2ca25b83b3cd6e83525273a8
jakechaley <jakechaley@gmail.com>
2022-09-14 12:34:56 -0700

    Practice basic tools with Java

--- src/hello.java
+++ src/hello.java
@@ -11,13 +11,38 @@ public class hello {
         String name = "Jake";
         boolean isHome = false;
 
-        System.out.println(num);
-        System.out.println(doubley);
-        System.out.println(name);
-        System.out.println(firstInitial);
-        System.out.println(isHome);
+//        System.out.println(num);
+//        System.out.println(doubley);
+//        System.out.println(name);
+//        System.out.println(firstInitial);
+//        System.out.println(isHome);
 
+        if (num < 18) {
+            System.out.println("You are not an adult");
+        }
 
+        while(num < 18) {
+            System.out.println("You are not an adult");
+            // num = num + 1;
+            num++;
+        }
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
+
+        if (name.equals("Jake")) {
+            System.out.println("Happy Birthday");
+        } else {
+            System.out.println("Have a nice day");
+        }
+
+        int x = 0;
+
+        do {
+            System.out.println("Happy Wednesday");
+            x++;
+        } while(x < 10);
 
     }
 }
